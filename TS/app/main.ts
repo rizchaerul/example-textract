60d8c08e9c37e930f294e664ca8e027e09ed5198
Chaerul Rizky <riz.chaerul@gmail.com>
2022-05-25 11:51:26 +0700

    implement textract

--- TS/app/main.ts
+++ TS/app/main.ts
@@ -1,1 +1,77 @@
-console.log("hello");
+import {
+    AnalyzeDocumentCommand,
+    AnalyzeExpenseCommand,
+    TextractClient,
+} from "@aws-sdk/client-textract";
+import { readFile, writeFile } from "fs/promises";
+
+(async () => {
+    const file = await readFile("./samples/nhs.pdf");
+    // const file = await readFile("./samples/jb-payslip.pdf");
+    // const file = await readFile("./samples/resource-solutions-payslip.pdf");
+
+    const client = new TextractClient({
+        region: "us-west-2",
+    });
+
+    const docCommand = new AnalyzeDocumentCommand({
+        Document: { Bytes: file },
+        // See https://docs.aws.amazon.com/textract/latest/dg/API_AnalyzeDocument.html
+        FeatureTypes: ["TABLES", "FORMS", "QUERIES"],
+        QueriesConfig: {
+            Queries: [
+                {
+                    Text: "Who's the employee?",
+                },
+            ],
+        },
+    });
+
+    const expenseCommand = new AnalyzeExpenseCommand({
+        // See https://docs.aws.amazon.com/textract/latest/dg/API_AnalyzeExpense.html
+        Document: { Bytes: file },
+    });
+
+    let AnalyzeDocOutput = await client.send(docCommand);
+    await writeFile(
+        "./results/document-api/raw.json",
+        JSON.stringify(AnalyzeDocOutput, null, 2)
+    );
+
+    // Delete geometry from object
+    AnalyzeDocOutput.Blocks?.forEach((b) => {
+        delete b.Geometry;
+    });
+    await writeFile(
+        "./results/document-api/raw-no-geometry.json",
+        JSON.stringify(AnalyzeDocOutput, null, 2)
+    );
+
+    let expenseCommandResult = await client.send(expenseCommand);
+    await writeFile(
+        "./results/expense-api/raw.json",
+        JSON.stringify(expenseCommandResult, null, 2)
+    );
+
+    // Delete geometry from object
+    expenseCommandResult.ExpenseDocuments?.forEach((e) => {
+        e.SummaryFields?.forEach((s) => {
+            delete s.LabelDetection?.Geometry;
+            delete s.ValueDetection?.Geometry;
+        });
+
+        e.LineItemGroups?.forEach((l) =>
+            l.LineItems?.forEach((l) =>
+                l.LineItemExpenseFields?.forEach((l) => {
+                    delete l.LabelDetection?.Geometry;
+                    delete l.ValueDetection?.Geometry;
+                })
+            )
+        );
+    });
+
+    await writeFile(
+        "./results/expense-api/raw-no-geometry.json",
+        JSON.stringify(expenseCommandResult, null, 2)
+    );
+})();
